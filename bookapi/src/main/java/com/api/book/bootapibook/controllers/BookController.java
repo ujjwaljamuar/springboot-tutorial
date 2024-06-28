c1c1fd8ee31c3a0bd7cac64c916b5add98e21001
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2024-06-28 20:48:17 +0530

    added update method using putmapping

--- bookapi/src/main/java/com/api/book/bootapibook/controllers/BookController.java
+++ bookapi/src/main/java/com/api/book/bootapibook/controllers/BookController.java
@@ -1,11 +1,14 @@
 package com.api.book.bootapibook.controllers;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 //import org.springframework.stereotype.Controller;
 //import org.springframework.web.bind.annotation.ResponseBody;
@@ -21,29 +24,42 @@ import com.api.book.bootapibook.services.BookServices;
 public class BookController {
 //	@RequestMapping(value="/getBooks", method = RequestMethod.GET)
 //	@ResponseBody
-	
+
 	@Autowired
 	private BookServices bookService;
-	
+
 	@GetMapping("/getbooks")
-	public ArrayList<Book> getBooks() {
-		
+	public List<Book> getBooks() {
+
 		// jackson auto converts objects into json while returning in body
 		return bookService.getAllBooks();
 	}
-	
+
 	@GetMapping("/book/{id}")
 	public Book getBook(@PathVariable("id") int id) {
 		return bookService.getBookById(id);
 	}
-	
+
 	@PostMapping("book/new")
 	// whatever json data you put in post request body will go in Book object
 	public Book addBook(@RequestBody Book newBook) {
 		Book addedBook = bookService.addBook(newBook);
-		
+
 		return addedBook;
 	}
-	
-	
+
+	@DeleteMapping("/book/{id}")
+	public Book deleteBook(@PathVariable("id") int id) {
+		Book deletedBook = bookService.deleteBookById(id);
+
+		return deletedBook;
+	}
+
+	@PutMapping("book/{id}")
+	public Book updateBook(@PathVariable("id") int id, @RequestBody Book newBook) {
+		Book updatedBook = bookService.updateBookById(id, newBook);
+
+		return updatedBook;
+	}
+
 }
--- bookapi/src/main/java/com/api/book/bootapibook/services/BookServices.java
+++ bookapi/src/main/java/com/api/book/bootapibook/services/BookServices.java
@@ -1,6 +1,8 @@
 package com.api.book.bootapibook.services;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Component;
 
@@ -8,33 +10,57 @@ import com.api.book.bootapibook.entities.Book;
 
 @Component
 public class BookServices {
-	private static ArrayList<Book> bookList = new ArrayList<>();
-	
-	// static block always executed first, 
+	private static List<Book> bookList = new ArrayList<>();
+
+	// static block always executed first,
 	// so that our controller always return atleast one object
 	static {
 		bookList.add(new Book(0, "Meena", "Luffy"));
 		bookList.add(new Book(1, "Meena1", "Luffy1"));
 		bookList.add(new Book(2, "Meena2", "Luffy2"));
 	}
-	
+
 	// get all books
-	public ArrayList<Book> getAllBooks(){
+	public List<Book> getAllBooks() {
 		return bookList;
 	}
-	
+
 	// get book by id
 	public Book getBookById(int id) {
 		Book book = null;
 		book = bookList.stream().filter(e -> e.getId() == id).findFirst().get();
-		
+
 		return book;
 	}
-	
+
 	// adding new book
 	public Book addBook(Book newBook) {
 		bookList.add(newBook);
-		
+
 		return newBook;
+	}
+
+	public Book deleteBookById(int id) {
+//		Book book = bookList.stream().filter(e -> e.getId() == id).findFirst().get();
+//		bookList.remove(book);
+
+		Book b = getBookById(id);
+
+		// or change the whole booklist
+		bookList = bookList.stream().filter(book -> book.getId() != id).collect(Collectors.toList());
+
+		return b;
+	}
+
+	public Book updateBookById(int id, Book newBookObj) {
+		bookList = bookList.stream().map(book -> {
+			if (book.getId() == id) {
+				book.setTitle(newBookObj.getTitle());
+				book.setAuthor(newBookObj.getAuthor());
+			}
+			return book;
+		}).collect(Collectors.toList());
+
+		return newBookObj;
 	}
 }
